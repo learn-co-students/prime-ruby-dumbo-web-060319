69cb982d3653bd92bbee04ee4493756cd940db7e
Alexander Schelchere <ascellie@gmail.com>
2019-05-11 21:30:31 -0400

    I know I'm close

--- prime.rb
+++ prime.rb
@@ -8,11 +8,16 @@ def prime?(num)
 
     # Iterate through our array
     arr.each do |i|
+        # If "i" is not a prime number, we don't know her.
+        if not_prime_arr.include?(i)
+            next
+        end
+
         # compare the number to every other number in the array
         arr.each do |j|
             # continue if they're the same number
             if i == j
-                continue
+                next
             end
             # if our number, j, is divisible by our controlled number, i
             if j % i == 0
@@ -28,3 +33,4 @@ end
 # This is what I have so far, We'll then have to add more functionality to in the top half of our loop.
 # that checks to see if "i" is in the array not_prime_number
 # In that case, we will just continue. "i" will only be prime numbers.
+# JAVASCRIPT MIND - WHOOPS! Continue is Next
