1d0caf5be2084b4b63f179c1837d91265f415339
github username <github email address>
2019-05-20 18:16:02 -0400

    A bit verbose but clear and passing :-)

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+    list_of_divisors = (1..100).to_a
+    result_list = []
+    list_of_divisors.each do |divisor|
+        if integer % divisor == 0
+            factor = integer / divisor
+            result_list.push(factor)
+        end
+    end
+    if result_list.size <= 2 && integer > 1
+        return true
+    else
+        return false
+    end
+end
