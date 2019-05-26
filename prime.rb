050d07a53e27d923897976e78e5594d31efca88a
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-26 03:54:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(a)
+  if a <= 1
+    return false
+  elsif a ==2 || a==3
+    return true
+  else
+    for num in 2..(a-1)
+    if a % num == 0 
+      return false
+    end
+  end
+  true
+  end
+end
+ 
